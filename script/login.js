848e8e88713af7524f1d91cbe1e5b6e10711612d
johanssp <johans1996.18@gmail.com>
2023-11-12 20:39:06 -0300

    arreglo de error

--- script/login.js
+++ script/login.js
@@ -33,11 +33,11 @@ const usuarioValido = () => {
     Swal.fire("Bienvenido/a " + user.nombre);
     // en este if se valida la contraseña
     if (user.clave === valorInputs().inputPassword.trim().toLowerCase()) {
-      // si existe el usuario se devuelve un alert con el nombre y apellido y por consola los datos y se guarda en local storange
+      // si existe el usuario  se guarda en local storange
       const correo = localStorage.setItem("correo", valorInputs().inputEmail);
       const gg = JSON.stringify(user);
       const clav2 = localStorage.setItem("clave", valorInputs().inputPassword);
-      /*const gt = JSON.stringify(user.clave);*/
+      const gt = JSON.stringify(user.clave);
       Swal.fire("Bienvenido/a " + user.nombre);
     } else {
       document.getElementById("mensaje").innerHTML = "Contraseña incorrecta";
