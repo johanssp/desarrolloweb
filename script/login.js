197c2595cd91276c146ea0536168b773efbe3ee3
johanssp <johans1996.18@gmail.com>
2023-11-12 20:36:23 -0300

    promesa y libreria

--- script/login.js
+++ script/login.js
@@ -1,5 +1,3 @@
-//pre-entrega3
-
 const form = document.getElementById("form");
 form.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -17,32 +15,44 @@ const valorInputs = () => {
     inputPassword,
   };
 };
+let arrayUsers;
+const usuario2 = fetch("/script/usuarios.json")
+  .then((res) => res.json())
+  .then((data) => {
+    arrayUsers = data;
+  });
 
 const usuarioValido = () => {
-  fetch("/script/usuarios.json")
-    .then((res) => res.json())
-    .then((data) => {});
-  const user = usuariosMock.find((user) => {
+  const user = arrayUsers.find((user) => {
     return user.email === valorInputs().inputEmail.trim().toLowerCase();
 
     // devuelve al usuario que coincide con el correo ingresado o devuelve undefined
   });
   // validamos que exista el usuario
   if (user) {
+    Swal.fire("Bienvenido/a " + user.nombre);
     // en este if se valida la contraseña
     if (user.clave === valorInputs().inputPassword.trim().toLowerCase()) {
       // si existe el usuario se devuelve un alert con el nombre y apellido y por consola los datos y se guarda en local storange
       const correo = localStorage.setItem("correo", valorInputs().inputEmail);
       const gg = JSON.stringify(user);
       const clav2 = localStorage.setItem("clave", valorInputs().inputPassword);
-      const gt = JSON.stringify(user.clave);
-      console.log(gg + gt);
-      alert("Bienvenido/a " + user.nombre);
-      console.log(user);
+      /*const gt = JSON.stringify(user.clave);*/
+      Swal.fire("Bienvenido/a " + user.nombre);
     } else {
       document.getElementById("mensaje").innerHTML = "Contraseña incorrecta";
+      Swal.fire({
+        icon: "error",
+        title: "error",
+        text: " ingrese un dato valido",
+      });
     }
   } else {
     document.getElementById("mensaje").innerHTML = "Correo no existe";
+    Swal.fire({
+      icon: "error",
+      title: "error",
+      text: " ingrese un dato valido",
+    });
   }
 };
